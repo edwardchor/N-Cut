355444d15f8c66bb3f30efc9478b43a2137c842c
ÂÇ®‰ª≤Â∞ß <czy8023@me.com>
2015-09-26 19:38:15 +0800

    Initial Commit

---
+++ N-cut/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  N-cut
+//
+//  Created by 储仲尧 on 9/26/15.
+//  Copyright © 2015 Uopia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
