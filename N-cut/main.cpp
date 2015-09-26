4432c5c105c50bdbb700992f27b2c5bf170814d3
储仲尧 <czy8023@me.com>
2015-09-26 19:54:50 +0800

    temp

--- N-cut/main.cpp
+++ N-cut/main.cpp
@@ -1,6 +1,7 @@
 //
 //  main.cpp
 //  N-cut
+//  This is a simple simulation of normalised cut of small graphs.
 //
 //  Created by 储仲尧 on 9/26/15.
 //  Copyright © 2015 Uopia. All rights reserved.
@@ -14,9 +15,7 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    ofstream myfile;
-    myfile.open ("example.txt");
-    myfile << "Writing this to a file.\n";
-    myfile.close();
-    return 0;
+    ifstream inputData(argv[0]);
+    ofstream outputData("result.txt");
+    
 }
