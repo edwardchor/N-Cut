092053200d0d1baeb4fb120c7ae1f40e5c19c2d4
储仲尧 <czy8023@me.com>
2015-09-26 21:22:37 +0800

    temp3

--- N-cut/main.cpp
+++ N-cut/main.cpp
@@ -11,17 +11,32 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#define MAX_LAYERS 2
 
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    ifstream inputData("example.txt");
+    ifstream inputData("graph.txt");
     ofstream outputData("result.txt");
     string line;
-    int graph[10][10];
-    while(!inputData.eof()){
+    int vertex[10][10];
+    int result[MAX_LAYERS][10][10];
+    int i,j,k;
+    int row=0;
+    while(row<10){
         getline(inputData,line);
-        
+        for(i=0;i<10;i++){
+            vertex[row][i]=line[i]-'0';
+            cout<<vertex[row][i];
+        }
+        cout<<endl;
+        row++;
     }
+    for(j=0;j<10;j++){
+        for(k=0;k<10;k++){
+            
+        }
+    }
+    
     return 0;
 }
